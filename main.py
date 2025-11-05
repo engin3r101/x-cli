05ded40ac67b610ee0e019867dec9d1f6eb18735
Varun Dunna <varun.dunna2524@gmail.com>
2025-11-05 18:15:39 +0530

    temp-changes

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from fastapi import FastAPI, Header, HTTPException
-from x_service import TwitterService
+# from fastapi import FastAPI, Header, HTTPException
+# from x_service import TwitterService
 
-app = FastAPI(title="Local Twitter Bot")
+# app = FastAPI(title="Local Twitter Bot")
 
-twitter = TwitterService()
+# twitter = TwitterService()
 
-# Set a local API key for security
-API_AUTH_KEY = "local-secret-key"
+# # Set a local API key for security
+# API_AUTH_KEY = "local-secret-key"
 
-@app.post("/tweet")
-def post_tweet(message: str, x_api_key: str = Header(None)):
-    if x_api_key != API_AUTH_KEY:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+# @app.post("/tweet")
+# def post_tweet(message: str, x_api_key: str = Header(None)):
+#     if x_api_key != API_AUTH_KEY:
+#         raise HTTPException(status_code=401, detail="Unauthorized")
 
-    return twitter.post_tweet(message)
+#     return twitter.post_tweet(message)
